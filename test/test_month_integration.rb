0ca2225bdbf1cdd77430fcab86117cfc8fbdb603
Allison Higginbotham <higgintop@gmail.com>
2015-04-23 12:57:33 -0500

    Changed format of expected string in two test cases

---
+++ test/test_month_integration.rb
@@ -0,0 +1,361 @@
+require_relative 'helper'
+
+class TestCalIntegration < Minitest::Test
+
+  ########################################
+  # Test each month of 2015 to_s
+  ########################################
+
+  def test_jan_2015
+    output = `./cal.rb 01 2015`
+    expected = <<EOS
+    January 2015
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+
+  def test_feb_2015
+    output = `./cal.rb 02 2015`
+    expected = <<EOS
+   February 2015
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_march_2015
+    output = `./cal.rb 03 2015`
+    expected = <<EOS
+     March 2015
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+
+  def test_april_2015
+    output = `./cal.rb 04 2015`
+    expected = <<EOS
+     April 2015
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29 30
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_may_2015
+    output = `./cal.rb 05 2015`
+    expected = <<EOS
+      May 2015
+Su Mo Tu We Th Fr Sa
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+31
+
+EOS
+    assert_equal expected, output
+  end
+
+
+  def test_june_2015
+    output = `./cal.rb 06 2015`
+    expected = <<EOS
+     June 2015
+Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30
+
+EOS
+    assert_equal expected, output
+  end
+
+
+  def test_july_2015
+    output = `./cal.rb 07 2015`
+    expected = <<EOS
+     July 2015
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_august_2015
+    output = `./cal.rb 08 2015`
+    expected = <<EOS
+    August 2015
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+
+  def test_september_2015
+    output = `./cal.rb 09 2015`
+    expected = <<EOS
+   September 2015
+Su Mo Tu We Th Fr Sa
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29 30
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_october_2015
+    output = `./cal.rb 10 2015`
+    expected = <<EOS
+    October 2015
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+
+  def test_november_2015
+    output = `./cal.rb 11 2015`
+    expected = <<EOS
+   November 2015
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_december_2015
+    output = `./cal.rb 12 2015`
+    expected = <<EOS
+   December 2015
+Su Mo Tu We Th Fr Sa
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+
+  #########################################
+  # Leap year tests
+  #########################################
+
+  def test_leap_year_divisible_by_4
+    output = `./cal.rb 02 2012`
+    expected = <<EOS
+   February 2012
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_leap_year_divisible_by_100
+    output = `./cal.rb 02 3000`
+    expected = <<EOS
+   February 3000
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_leap_year_divisible_by_400
+    output = `./cal.rb 02 2000`
+    expected = <<EOS
+   February 2000
+Su Mo Tu We Th Fr Sa
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29
+
+EOS
+    assert_equal expected, output
+  end
+
+
+
+
+  #########################################
+  # min and max date range tests
+  #########################################
+  def test_min_date
+    output = `./cal.rb 01 1800`
+    expected = <<EOS
+    January 1800
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_max_date
+    output = `./cal.rb 12 3000`
+    expected = <<EOS
+   December 3000
+Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_before_min_date
+    output = `./cal.rb 01 1700`
+    expected = "Date not in acceptable format/range"
+    assert_equal expected, output
+  end
+
+  def test_after_max_date
+    output = `./cal.rb 01 3001`
+    expected = "Date not in acceptable format/range"
+    assert_equal expected, output
+  end
+
+  ###########################################
+  # Error cases: bad date formats
+  ###########################################
+
+  def test_bad_date_format_a
+    output = `./cal.rb April 2014`
+    expected = <<EOS
+Date not in acceptable format/range
+EOS
+    assert_equal output, expected
+  end
+
+  def test_bad_date_format_b
+    output = `./cal.rb 2014 04`
+    expected = <<EOS
+Date not in acceptable format/range
+EOS
+    assert_equal expected, output
+  end
+
+  ############################################
+  # Special cases
+  ############################################
+
+  def test_six_week_month
+    output = `./cal.rb 03 2014`
+    expected = <<EOS
+     March 2014
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+  def test_month_starting_on_sunday
+    output = `./cal.rb 01 2012`
+    expected = <<EOS
+    January 2012
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+
+EOS
+    assert_equal expected, output
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
