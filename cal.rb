291c71c5793143506b76a027beb569f79769fa12
Allison Higginbotham <higgintop@gmail.com>
2015-04-24 10:07:15 -0500

    removed comments from cal.rb

--- cal.rb
+++ cal.rb
@@ -3,8 +3,6 @@
 require_relative 'lib/Month'
 
 # This is ruby file where we will use Month, Day, Year and call to_s
-# month = Month.new(ARGV[0], ARGV[1])
-# puts month.to_s
 
 userInputMonth = ARGV[0]
 userInputYear = ARGV[1]
