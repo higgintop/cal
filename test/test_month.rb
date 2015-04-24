6383fe0a74f5dc0bc2f38725e6b62d4886f981b7
Allison Higginbotham <higgintop@gmail.com>
2015-04-24 10:41:47 -0500

    Added unit tests

--- test/test_month.rb
+++ test/test_month.rb
@@ -35,5 +35,35 @@ class TestMonth < Minitest::Test
     assert_equal m.day_count, 29
   end
 
+  def test_to_s_method_starting_on_sunday
+    m = Month.new(03, 2015)
+    expected = <<EOS
+     March 2015
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_to_s_method_starting_on_saturday
+    m = Month.new(03, 3000)
+    expected = <<EOS
+     March 3000
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+
+EOS
+    assert_equal expected, m.to_s
+  end
 
 end
