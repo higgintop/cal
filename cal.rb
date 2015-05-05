7c66b1f81caf66263f05de74ed497e792a0716d2
Allison Higginbotham <higgintop@gmail.com>
2015-05-05 09:10:01 -0500

    Added year.to_s to cal.rb

--- cal.rb
+++ cal.rb
@@ -4,8 +4,15 @@ require_relative 'lib/Month'
 
 # This is ruby file where we will use Month, Day, Year and call to_s
 
-userInputMonth = ARGV[0]
-userInputYear = ARGV[1]
 
-month = Month.new(userInputMonth, userInputYear)
-puts month.to_s
+if !ARGV[0].nil? and !ARGV[1].nil?
+  month = Month.new(ARGV[0], ARGV[1])
+  puts month.to_s
+end
+
+if !ARGV[0].nil? and ARGV[1].nil?
+  year = Year.new(ARGV[0])
+  puts year.to_s
+end
+
+
